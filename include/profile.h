88e6443cc885dc9df2e994ecc80a7fb2705a499d
Pavel <p.a.zhdanov@mai.ru>
2021-04-08 23:56:33 +0300

    Implement profiler
    Don't use UNIQ_ID from the previous assignment.
    Implement it here again

---
+++ include/profile.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <string>
+
+using namespace std;
+using namespace std::chrono;
+
+class LogDuration {
+public:
+	LogDuration(const string &msg):
+		message(msg + ": "),
+		start(steady_clock::now())
+	{
+		// Empty constructor
+	}
+	~LogDuration()
+	{
+		auto finish = steady_clock::now();
+		auto duration = finish - start;
+		cerr << message
+			<< duration_cast<milliseconds>(duration).count()
+			<< " ms" << endl;
+	}
+
+private:
+	string message;
+	steady_clock::time_point start;
+};
+
+#define UNIQ_ID_IMPL(linenum) _a_local_var_##linenum
+#define UNIQ_ID(linenum) UNIQ_ID_IMPL(linenum)
+
+#define LOG_DURATION(message) \
+	LogDuration UNIQ_ID(__LINE__)(message);
