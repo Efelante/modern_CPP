196232dce3733c5e97f217a26721e8ce9ab6499b
Pavel Zhdanov <p.a.zhdanov@mai.ru>
2021-05-21 11:25:37 +0300

    Add complexity estimates

--- red/w2_ebook/ebook.cpp
+++ red/w2_ebook/ebook.cpp
@@ -11,26 +11,27 @@ public:
 	ReadingManager() {};
 
 	void Read(int user_id, int page_count) {
-		if (user_page_counts.count(user_id) != 0){
-			int old_count = user_page_counts[user_id];
-			page_counts_user_counts[old_count] -= 1;
+		if (user_page_counts.count(user_id) != 0){		// Log Q
+			int old_count = user_page_counts[user_id];	// Log Q
+			page_counts_user_counts[old_count] -= 1;	// Log Q
 		}
-		user_page_counts[user_id] = page_count;
-		page_counts_user_counts[page_count] += 1;
+		user_page_counts[user_id] = page_count;			// Log Q
+		page_counts_user_counts[page_count] += 1;		// Log Q
 	}
 
 	double Cheer(int user_id) const {
-		if (user_page_counts.count(user_id) == 0) {
+		if (user_page_counts.count(user_id) == 0) {		// Log Q
 			return 0;
 		}
-		const int user_count = GetUserCount();
+		const int user_count = GetUserCount();			// Constant
 		if (user_count == 1) {
 			return 1;
 		}
 
-		int page_count = user_page_counts.at(user_id);
+		int page_count = user_page_counts.at(user_id);		// Log Q
 		int sum = 0;
-		for (auto it = page_counts_user_counts.begin(); it->first != page_count; it = next(it)){
+		// Accumulate has linear complexity of the distance between first and last
+		for (auto it = page_counts_user_counts.begin(); it->first != page_count; it = next(it)){	// Q (?)
 			sum += it->second;	
 		}
 		return sum * 1.0 / (user_count - 1);
