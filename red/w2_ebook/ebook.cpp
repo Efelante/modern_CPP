ac5db7e60afe02ecc3fa5ed56c756810ec3143e9
Pavel Zhdanov <paul.alex.zhdanov@gmail.com>
2021-05-21 10:22:25 +0300

    Use maps instead of vectors
    Status: doesn't work

--- red/w2_ebook/ebook.cpp
+++ red/w2_ebook/ebook.cpp
@@ -1,76 +1,48 @@
 #include <iomanip>
 #include <iostream>
-#include <vector>
+#include <map>
+#include <algorithm>
 #include <utility>
+#include <numeric>
 
 using namespace std;
 
 class ReadingManager {
 public:
-  ReadingManager()
-      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+	ReadingManager() {};
 
-  void Read(int user_id, int page_count) {
-    if (user_page_counts_[user_id] == 0) {
-      AddUser(user_id);
-    }
-    user_page_counts_[user_id] = page_count;
-    int& position = user_positions_[user_id];
-    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-      SwapUsers(position, position - 1);
-    }
-  }
+	void Read(int user_id, int page_count) {
+		user_page_counts_[user_id] = page_count;
+	}
 
-  double Cheer(int user_id) const {
-    if (user_page_counts_[user_id] == 0) {
-      return 0;
-    }
-    const int user_count = GetUserCount();
-    if (user_count == 1) {
-      return 1;
-    }
-    const int page_count = user_page_counts_[user_id];
-    int position = user_positions_[user_id];
-    while (position < user_count &&
-      user_page_counts_[sorted_users_[position]] == page_count) {
-      ++position;
-    }
-    if (position == user_count) {
-        return 0;
-    }
-    // По умолчанию деление целочисленное, поэтому
-    // нужно привести числитель к типу double.
-    // Простой способ сделать это — умножить его на 1.0.
-    return (user_count - position) * 1.0 / (user_count - 1);
-  }
+double Cheer(int user_id) const {
+	if (user_page_counts_.count(user_id) == 0) {
+		return 0;
+	}
+	const int user_count = GetUserCount();
+	if (user_count == 1) {
+		return 1;
+	}
+	int page_count = user_page_counts_.at(user_id);
+	auto lbound = page_counts_user_counts.lower_bound(page_count);
+	auto part = accumulate(page_counts_user_counts.begin(),
+			lbound,
+			0,
+			[](int sum, const pair<int, int> &item){
+				return sum + item.second;
+			}
+	);
+	return part * 1.0 / (user_count - 1);
+
+}
 
 private:
-  // Статическое поле не принадлежит какому-то конкретному
-  // объекту класса. По сути это глобальная переменная,
-  // в данном случае константная.
-  // Будь она публичной, к ней можно было бы обратиться снаружи
-  // следующим образом: ReadingManager::MAX_USER_COUNT.
-  static const int MAX_USER_COUNT_ = 100'000;
+	map<int, int> page_counts_user_counts;
+	map<int, int> user_page_counts_;
 
-  vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-  vector<int> user_positions_; // позиции в векторе sorted_users_
-
-  int GetUserCount() const {
-    return sorted_users_.size();
-  }
-  void AddUser(int user_id) {
-    sorted_users_.push_back(user_id);
-    user_positions_[user_id] = sorted_users_.size() - 1;
-  }
-  void SwapUsers(int lhs_position, int rhs_position) {
-    const int lhs_id = sorted_users_[lhs_position];
-    const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-  }
+	int GetUserCount() const {
+		return user_page_counts_.size();
+	}
 };
 
 
