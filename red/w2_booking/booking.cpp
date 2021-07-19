85a1f88efc58550d1526dc967efd94a44f6d1d74
Pavel Zhdanov <paul.alex.zhdanov@gmail.com>
2021-07-19 10:55:48 +0300

    Optimize
    Wrong answer

--- red/w2_booking/booking.cpp
+++ red/w2_booking/booking.cpp
@@ -34,19 +34,25 @@ public:
 			auto start_time = current_time - 86400;
 			auto start_item = hotel_data.upper_bound(start_time);
 			unsigned int rooms = 0;
-			for (auto item = start_item; item != hotel_data.end(); item = next(item)){
-				rooms += item->second.first;
-			}
+			rooms = hotel_data[current_time].first - start_item->first;
+			//for (auto item = start_item; item != hotel_data.end(); item = next(item)){
+			//	rooms += item->second.first;
+			//}
 			res = rooms;
 		}
 		return res;
 	}
 
-    void Book(const string &hotel_name, long long int time, unsigned int client_id, unsigned int room_count)
+	void Book(const string &hotel_name, long long int time, unsigned int client_id, unsigned int room_count)
 	{
-		current_time = time;
-		storage[hotel_name][time].first += room_count;
-		storage[hotel_name][time].second.insert(client_id); 
+		if (storage[hotel_name].size() != 0 && time != current_time){
+			storage[hotel_name][time].first += storage[hotel_name][current_time].first;
+			//storage[hotel_name][time].second.insert(storage[hotel_name][current_time].second); 
+		} else {
+			current_time = time;
+			storage[hotel_name][time].first += room_count;
+			storage[hotel_name][time].second.insert(client_id); 
+		}
 	}
 private:
 	long long int current_time = 0;
@@ -56,45 +62,45 @@ private:
 
 
 int main() {
-  // Для ускорения чтения данных отключается синхронизация
-  // cin и cout с stdio,
-  // а также выполняется отвязка cin от cout
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+	// Для ускорения чтения данных отключается синхронизация
+	// cin и cout с stdio,
+	// а также выполняется отвязка cin от cout
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-  BookingSystem bsystem;
+	BookingSystem bsystem;
 
-  int query_count;
-  cin >> query_count;
+	int query_count;
+	cin >> query_count;
 
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
+	for (int query_id = 0; query_id < query_count; ++query_id) {
+		string query_type;
+		cin >> query_type;
 
-    if (query_type == "BOOK") {
-      long long int time;
-      cin >> time;
-	  string hotel_name;
-	  cin >> hotel_name;
-	  unsigned int client_id;
-	  cin >> client_id;
-	  unsigned int room_count;
-	  cin >> room_count;
+		if (query_type == "BOOK") {
+			long long int time;
+			cin >> time;
+			string hotel_name;
+			cin >> hotel_name;
+			unsigned int client_id;
+			cin >> client_id;
+			unsigned int room_count;
+			cin >> room_count;
 
-      bsystem.Book(hotel_name, time, client_id, room_count);
-    } else if (query_type == "CLIENTS") {
-	  string hotel_name;
-	  cin >> hotel_name;
-      cout << bsystem.Clients(hotel_name) << "\n";
-    } else if (query_type == "ROOMS") {
-	  string hotel_name;
-	  cin >> hotel_name;
-      cout << bsystem.Rooms(hotel_name) << "\n";
-    } else {
-		// Unknown query type
+			bsystem.Book(hotel_name, time, client_id, room_count);
+		} else if (query_type == "CLIENTS") {
+			string hotel_name;
+			cin >> hotel_name;
+			cout << bsystem.Clients(hotel_name) << "\n";
+		} else if (query_type == "ROOMS") {
+			string hotel_name;
+			cin >> hotel_name;
+			cout << bsystem.Rooms(hotel_name) << "\n";
+		} else {
+			// Unknown query type
+		}
+
 	}
 
-  }
-
-  return 0;
+	return 0;
 }
