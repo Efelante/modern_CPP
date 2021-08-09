057ab68c13e4c8de26b03c2845c3f0edbd0fb1fc
Pavel Zhdanov <p.a.zhdanov@mai.ru>
2021-08-09 17:27:12 +0300

    Add offset search to the BOOK command
    Find offset for each hotel after each BOOK command.
    There should be one update for the hotel. Update in ROOMS or CLIENTS
    leads to #15/17, in BOOK - #6/17

--- red/w2_booking/booking.cpp
+++ red/w2_booking/booking.cpp
@@ -18,18 +18,10 @@ public:
 	{
 		unsigned int res = 0;
 		if (hotel_to_clients.count(hotel_name) != 0){
-			long long int start_time = current_time - 86400;
-			auto &time = hotel_to_time.at(hotel_name);
-			auto &clients = hotel_to_clients.at(hotel_name);
-			auto start_item = find_if(time.begin(), time.end(), 
-					[start_time](long long int time){return time > start_time;});
-			if (start_item != time.end()){
-				auto offset = start_item - time.begin();
-				set<unsigned int> unique_clients(clients.begin() + offset, clients.end());
-				res = unique_clients.size();
-				clients.erase(clients.begin(), clients.begin() + offset);
-				time.erase(time.begin(), start_item);
-			}
+			auto clients = hotel_to_clients.at(hotel_name);
+			auto offset = hotel_to_offset.at(hotel_name);
+			set<unsigned int> unique_clients(clients.begin() + offset, clients.end());
+			res = unique_clients.size();
 		}
 		return res;
 	}
@@ -38,16 +30,10 @@ public:
 	{
 		unsigned int res = 0;
 		if (hotel_to_rooms.count(hotel_name) != 0){
-			auto hotel_data = hotel_to_rooms.at(hotel_name);
-			auto start_time = current_time - 86400;
-			auto start_item = hotel_data.upper_bound(start_time);
-			unsigned int rooms = 0;
-			if (start_item != hotel_data.end()){
-				for (auto item = start_item; item != hotel_data.end(); item = next(item)){
-					rooms += item->second;
-				}
-				res = rooms;
-			}
+			auto rooms = hotel_to_rooms.at(hotel_name);
+			auto offset = hotel_to_offset.at(hotel_name);
+			unsigned int rooms_count = accumulate(rooms.begin() + offset, rooms.end(), 0);
+			res = rooms_count;
 		}
 		return res;
 	}
@@ -55,14 +41,34 @@ public:
 	void Book(const string &hotel_name, long long int time, unsigned int client_id, unsigned int room_count)
 	{
 		current_time = time;
-		hotel_to_rooms[hotel_name][time] += room_count;
+		hotel_to_rooms[hotel_name].push_back(room_count);
 		hotel_to_time[hotel_name].push_back(time);
 		hotel_to_clients[hotel_name].push_back(client_id);
+		for (auto &hotel: hotel_to_time){
+			ClearData(hotel.first);
+		}
 	}
 
 private:
+	void ClearData (const string &hotel_name)
+	{
+		if (hotel_to_clients.count(hotel_name) != 0){
+			long long int start_time = current_time - 86400;
+			auto &time = hotel_to_time.at(hotel_name);
+			auto &offset = hotel_to_offset[hotel_name];
+			auto start_item = find_if(time.begin() + offset, time.end(), 
+					[start_time](long long int time){return time > start_time;});
+			if (start_item != time.end()){
+				offset = start_item - time.begin();
+			} else {
+				offset = time.size();
+			}
+		}
+
+	}
 	long long int current_time = 0;
-	map<string, map<long long int, unsigned int>> hotel_to_rooms;
+	map<string, int> hotel_to_offset;
+	map<string, deque<unsigned int>> hotel_to_rooms;
 	map<string, deque<long long int>> hotel_to_time;
 	map<string, deque<unsigned int>> hotel_to_clients;
 };
