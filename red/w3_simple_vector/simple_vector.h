fac9a3243a7ad2e1fa45bfdc30f60f1d6b74c764
pazhdanov <paul.alex.zhdanov@gmail.com>
2021-10-11 20:40:06 +0300

    Implement begin, end, partly ctors
    Add data and _end pointers

--- red/w3_simple_vector/simple_vector.h
+++ red/w3_simple_vector/simple_vector.h
@@ -6,19 +6,34 @@
 template <typename T>
 class SimpleVector {
 public:
-  SimpleVector();
-  explicit SimpleVector(size_t size);
+  SimpleVector()
+  {
+	  data = _end;
+  }
+  explicit SimpleVector(size_t size)
+  {
+	  data = new T[size];
+	  _end = data + size;
+  }
   ~SimpleVector();
 
-  T& operator[](size_t index);
+  T& operator[](size_t index)
+  {
+	  return data[index];
+  }
 
-  T* begin();
-  T* end();
+  T* begin() {return data};
+  T* end() { return _end};
 
-  size_t Size() const;
+  size_t Size() const
+  {
+	  return 
+  }
   size_t Capacity() const;
   void PushBack(const T& value);
 
 private:
+  T *data;
+  T *_end;
   // Добавьте поля для хранения данных вектора
 };
