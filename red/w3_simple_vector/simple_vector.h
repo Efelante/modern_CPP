836fd35bdb9e01d1699362dbdfc868c618add5d2
pazhdanov <paul.alex.zhdanov@gmail.com>
2021-10-20 20:41:05 +0300

    Implement methods for testConstruction

--- red/w3_simple_vector/simple_vector.h
+++ red/w3_simple_vector/simple_vector.h
@@ -8,32 +8,44 @@ class SimpleVector {
 public:
   SimpleVector()
   {
-	  data = _end;
+	  capacity = 0;
+	  data = NULL;
+	  _end = NULL;
   }
   explicit SimpleVector(size_t size)
   {
 	  data = new T[size];
 	  _end = data + size;
+	  capacity = size;
   }
-  ~SimpleVector();
+  ~SimpleVector()
+  {
+	  delete[] data;
+  }
 
   T& operator[](size_t index)
   {
 	  return data[index];
   }
 
-  T* begin() {return data};
-  T* end() { return _end};
+  T* begin() {return data;}
+  T* end() { return _end;}
+
+  const T* begin() const {return data;}
+  const T* end() const { return _end;}
 
   size_t Size() const
   {
-	  return 
+	  return (end() - begin()); 
   }
-  size_t Capacity() const;
+  size_t Capacity() const
+  {
+	  return capacity;
+  }
   void PushBack(const T& value);
 
 private:
   T *data;
   T *_end;
-  // Добавьте поля для хранения данных вектора
+  size_t capacity;
 };
