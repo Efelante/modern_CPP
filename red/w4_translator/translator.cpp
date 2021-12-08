8b7a53000e2ae596e957672687c68ea88f4a0df3
pazhdanov <paul.alex.zhdanov@gmail.com>
2021-12-08 08:50:49 +0300

    Implement translator

--- red/w4_translator/translator.cpp
+++ red/w4_translator/translator.cpp
@@ -11,8 +11,38 @@ public:
   string_view TranslateBackward(string_view target) const;
 
 private:
-  // ???
+  set<string> data;
+  map<string_view, string_view> forward;
+  map<string_view, string_view> backward;
 };
+
+void Translator::Add(string_view source, string_view target)
+{
+	auto sres = data.insert(string(source));
+	auto tres = data.insert(string(target));
+	forward[*sres.first] = *tres.first;
+	backward[*tres.first] = *sres.first;
+}
+
+string_view Translator::TranslateForward(string_view source) const
+{
+	auto sit = data.find(string(source));
+	if ((sit != data.end()) && forward.count(*sit)){
+		return forward.at(*sit);
+	} else {
+		return string("");
+	}
+}
+
+string_view Translator::TranslateBackward(string_view target) const
+{
+	auto tit = data.find(string(target));
+	if ((tit != data.end()) && backward.count(*tit)){
+		return backward.at(*tit);
+	} else {
+		return string("");
+	}
+}
 
 void TestSimple() {
   Translator translator;
