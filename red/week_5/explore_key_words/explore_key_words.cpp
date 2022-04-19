e214b9140bfc5ea0ed4380698ba205128d9fb74d
pazhdanov <paul.alex.zhdanov@gmail.com>
2022-04-19 09:24:58 +0300

    Move word exploration in a separate function

--- red/week_5/explore_key_words/explore_key_words.cpp
+++ red/week_5/explore_key_words/explore_key_words.cpp
@@ -11,24 +11,38 @@ struct Stats {
   void operator += (const Stats& other);
 };
 
+void Stats::operator += (const Stats& other)
+{
+	for (const auto &[word, cnt]: other.word_frequences){
+		word_frequences[word] += cnt;
+	}
+}
+
+Stats ExploreKeyword(const string &word, const string &pub){
+	Stats stats;
+	size_t word_size = word.size();
+	size_t match_pos = pub.find(word);
+	while(match_pos != string::npos){
+		if (0
+			|| ((match_pos == 0) && ((match_pos + word_size) == pub.size())) 						
+			|| ((match_pos == 0) && (pub[match_pos + word_size] ==  ' ')) 					 
+			|| ((pub[match_pos - 1] == ' ') && (pub[match_pos + word_size] == ' ')) 	 
+			|| ((pub[match_pos - 1] == ' ') && ((match_pos + word_size) == pub.size()))
+		   ){
+			stats.word_frequences[word] += 1;
+		}
+		match_pos = pub.find(word, match_pos + word_size);
+	}
+	return stats;
+}
+
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
   Stats stats;
   string pub;
+  //vector<future<Stats>> word_fut_stat;
   while (getline(input, pub)){
 	  for (const string &word: key_words){
-		  size_t word_size = word.size();
-		  size_t match_pos = pub.find(word);
-		  while(match_pos != string::npos){
-			  if (0
-				  || ((match_pos == 0) && ((match_pos + word_size) == pub.size())) 						
-			      || ((match_pos == 0) && (pub[match_pos + word_size] ==  ' ')) 					 
-			      || ((pub[match_pos - 1] == ' ') && (pub[match_pos + word_size] == ' ')) 	 
-			      || ((pub[match_pos - 1] == ' ') && ((match_pos + word_size) == pub.size()))
-				 ){
-				  stats.word_frequences[word] += 1;
-			  }
-			  match_pos = pub.find(word, match_pos + word_size);
-		  }
+		  stats += ExploreKeyword(word, pub);
 	  }
   }
   return stats;
