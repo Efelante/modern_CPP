7a9b112ffb4a0c47f3babcf2b101069fb92a102f
pazhdanov <paul.alex.zhdanov@gmail.com>
2022-04-25 10:20:26 +0300

    Distribute pubs between threads
    Multiple pubs for one thread.
    Each thread checks multiple publications
    for all words

--- red/week_5/explore_key_words/explore_key_words.cpp
+++ red/week_5/explore_key_words/explore_key_words.cpp
@@ -12,92 +12,6 @@
 
 using namespace std;
 
-template <typename Iterator>
-class Page {
-public:
-	Page(Iterator begin, Iterator end)
-	: _begin(begin), _end(end)
-	{
-		// Empty body
-	}
-
-	Iterator begin()
-	{
-		return _begin;
-	}
-
-	Iterator end()
-	{
-		return _end;
-	}
-
-	Iterator begin() const
-	{
-		return _begin;
-	}
-
-	Iterator end() const
-	{
-		return _end;
-	}
-
-
-	size_t size() const
-	{
-		return (_end - _begin);
-	}
-private:
-	Iterator _begin;
-	Iterator _end;
-};
-
-// Реализуйте шаблон класса Paginator
-
-template <typename Iterator>
-class Paginator {
-public:
-	Paginator(Iterator begin, Iterator end, size_t page_size)
-	{
-		Iterator page_start = begin;
-		Iterator page_end;
-		auto container_size = distance(begin, end);
-		size_t full_count = container_size / page_size;
-		for (size_t i = 0; i < full_count; ++i){
-			page_end = next(page_start, page_size);
-			pages.push_back(Page(page_start, page_end));
-			page_start = page_end;
-		}
-		if (container_size % page_size){
-			pages.push_back(Page(page_start, end));
-		}
-	}
-
-	size_t size() const
-	{
-		return pages.size();
-	}
-
-	auto begin()
-	{
-		return pages.begin();
-	}
-	
-	auto end()
-	{
-		return pages.end();
-	}
-	// Why don't I need const begin and end
-	// Because of auto?
-private:
-	vector<Page<Iterator>> pages;
-};
-
-template <typename C>
-auto Paginate(C& c, size_t page_size) {
-  return Paginator{c.begin(), c.end(), page_size};
-}
-
-
 struct Stats {
   map<string, int> word_frequences;
 
@@ -111,49 +25,61 @@ void Stats::operator += (const Stats& other)
 	}
 }
 
-Stats ExploreKeyword(const string &word, const string &pub){
+Stats ExplorePub(const set<string> &words, const string &pub){
 	Stats stats;
-	size_t word_size = word.size();
-	size_t match_pos = pub.find(word);
-	while(match_pos != string::npos){
-		if (0
-			|| ((match_pos == 0) && ((match_pos + word_size) == pub.size())) 						
-			|| ((match_pos == 0) && (pub[match_pos + word_size] ==  ' ')) 					 
-			|| ((pub[match_pos - 1] == ' ') && (pub[match_pos + word_size] == ' ')) 	 
-			|| ((pub[match_pos - 1] == ' ') && ((match_pos + word_size) == pub.size()))
-		   ){
-			stats.word_frequences[word] += 1;
+	for (const auto &word: words){
+		size_t word_size = word.size();
+		size_t match_pos = pub.find(word);
+		
+
+		while(match_pos != string::npos){
+			if (0
+					|| ((match_pos == 0) && ((match_pos + word_size) == pub.size())) 						
+					|| ((match_pos == 0) && (pub[match_pos + word_size] ==  ' ')) 					 
+					|| ((pub[match_pos - 1] == ' ') && (pub[match_pos + word_size] == ' ')) 	 
+					|| ((pub[match_pos - 1] == ' ') && ((match_pos + word_size) == pub.size()))
+			   ){
+				stats.word_frequences[word] += 1;
+			}
+			match_pos = pub.find(word, match_pos + word_size);
 		}
-		match_pos = pub.find(word, match_pos + word_size);
 	}
 	return stats;
 }
 
-Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
-  const int page_size = 4;
-  Stats stats;
-  string pub;
-  while (getline(input, pub)){
-  	  vector<future<Stats>> word_fut_stat;
-	  for (auto &page: Paginate(key_words, page_size)){
-		  word_fut_stat.push_back(
-	          async(
-		          [page, &pub](){
-			      	Stats stat;
-			      	for (const auto &word: page){
-			      	    stat += ExploreKeyword(word, pub);
-			      	}
-			      	return stat;
-			      }
-			  )
-		  );
+Stats ProcessPubs(const set<string> &key_words, const vector<string> &pubs)
+{
+	Stats res;
+	vector<future<Stats>> fstats;
+	for (const auto &pub: pubs){
+		fstats.push_back(async(ExplorePub, ref(key_words), ref(pub)));
+	}
+	for (auto &fstat: fstats){
+		res += fstat.get();
+	}
+	return res;
+}
 
-	  }
-	  for (future<Stats> &f: word_fut_stat){
-	     stats += f.get();
-	  }
-  }
-  return stats;
+Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
+	const int pub_limit = 2;
+	Stats stats;
+	vector<string> pubs;
+	string pub;
+	while (getline(input, pub)){
+		pubs.push_back(pub);
+		if (pubs.size() == pub_limit){
+			// Process pub_limit pubs
+			stats += ProcessPubs(key_words, pubs);
+			pubs.clear();
+		} else {
+			// Keep collecting pubs
+		}
+	}
+	if (pubs.size()){
+		// Process the last pubs
+		stats += ProcessPubs(key_words, pubs);
+	}
+	return stats;
 }
 
 void TestBasic() {
