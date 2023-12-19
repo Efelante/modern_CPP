bca89e9d0e2fd28e38b97752d7e973c2542e77f6
efelante <paul.alex.zhdanov@gmail.com>
2023-12-19 08:07:54 +0300

    Implement slow multithreading version
    Accumulate papers in multiple streams. One thread for one stream. This
    version is 2 times slower than a single thread version (34ms vs 17ms
    (out of 100ms))

--- red/week_5/explore_key_words/explore_key_words.cpp
+++ red/week_5/explore_key_words/explore_key_words.cpp
@@ -4,13 +4,14 @@
 #include <map>
 #include <set>
 #include <string>
+#include <future>
 
 using namespace std;
 
 struct Stats {
-  map<string, int> word_frequences;
+	map<string, int> word_frequences;
 
-  void operator += (const Stats& other);
+	void operator += (const Stats& other);
 };
 
 void Stats::operator += (const Stats& other)
@@ -20,7 +21,7 @@ void Stats::operator += (const Stats& other)
 	}
 }
 
-Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
+Stats ExploreKeyWordsSingleThread(const set<string>& key_words, istream& input) {
   Stats stats;
   string pub;
   while (input >> pub){
@@ -31,26 +32,56 @@ Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
   return stats;
 }
 
+Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
+	const int threads_num = 8;
+	Stats stats;
+	vector<future<Stats>> fstats;
+	vector<stringstream> paper_streams(threads_num);
+	string pub;
+	int i = 0;
+	while (getline(input, pub)){
+		paper_streams[i] << pub << "\n";
+		i = (i + 1) % threads_num;
+	}
+	for (auto &ss: paper_streams){
+		fstats.push_back(async(ExploreKeyWordsSingleThread, ref(key_words), ref(ss)));
+	}
+	for (auto &fs: fstats){
+		stats += fs.get();
+	}
+	return stats;
+}
+
 void TestBasic() {
-  const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
+	const int pnum = 100000;
 
-  stringstream ss;
-  ss << "this new yangle service really rocks\n";
-  ss << "It sucks when yangle isn't available\n";
-  ss << "10 reasons why yangle is the best IT company\n";
-  ss << "yangle rocks others suck\n";
-  ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+	const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
 
-  const auto stats = ExploreKeyWords(key_words, ss);
-  const map<string, int> expected = {
-    {"yangle", 6},
-    {"rocks", 2},
-    {"sucks", 1}
-  };
-  ASSERT_EQUAL(stats.word_frequences, expected);
+	stringstream ss;
+	for (int i = 0; i < pnum; ++i){
+		ss << "this new yangle service really rocks\n";
+		ss << "It sucks when yangle isn't available\n";
+		ss << "10 reasons why yangle is the best IT company\n";
+		ss << "yangle rocks others suck\n";
+		ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+	}
+
+	{LogDuration("Single thread");
+
+		const auto stats = ExploreKeyWords(key_words, ss);
+		const map<string, int> expected = {
+			{"yangle", pnum * 6},
+			{"rocks",  pnum * 2},
+			{"sucks",  pnum * 1}
+		};
+		for (const auto &[key, val]: stats.word_frequences){
+			cout << key << " " << val << endl;
+		}
+		ASSERT_EQUAL(stats.word_frequences, expected);
+	}
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestBasic);
+	TestRunner tr;
+	RUN_TEST(tr, TestBasic);
 }
